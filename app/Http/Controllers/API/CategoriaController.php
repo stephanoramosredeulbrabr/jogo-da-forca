22463a9209e05e0bc8024a9c8f153fec4026e63c
brendaab.2010@gmail.com <brendaab.2010@gmail.com>
2020-09-23 19:29:25 -0300

    Login

---
+++ app/Http/Controllers/API/CategoriaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Categoria extends Controller
+{
+
+    public function store(Request $request){
+
+
+    }
+
+    public function update(Request $request){
+
+        
+
+    }
+    
+    public function delete(Request $request){
+
+    }
+    
+}
