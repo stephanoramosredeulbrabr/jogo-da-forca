c7a6fde290ff250f5c41bc5242d15b65bcd4910a
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-09-23 20:29:36 -0300

    Fix Admin Migration

--- database/migrations/2020_09_23_230012_insert_admin_user.php
+++ database/migrations/2020_09_23_230012_insert_admin_user.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
 /**
  * Class InsertAdminUser
@@ -13,8 +14,12 @@ class InsertAdminUser extends Migration
      */
     public function up(): void
     {
-        User::factory()->create([
+        User::query()->create([
+            'name' => 'Administrador',
             'email' => 'admin@rede.ulbra.br',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
     }
 
