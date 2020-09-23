68b9c791846b187ce61174f587921d309899b681
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-09-23 20:56:34 -0300

    Heroku best practices

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\DatabaseTransactionMiddleware;
 use App\Http\Middleware\EncryptCookies;
 use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use App\Http\Middleware\RedirectIfAuthenticated;
@@ -42,6 +43,7 @@ class Kernel extends HttpKernel
         ValidatePostSize::class,
         TrimStrings::class,
         ConvertEmptyStringsToNull::class,
+        DatabaseTransactionMiddleware::class,
     ];
 
     /**
