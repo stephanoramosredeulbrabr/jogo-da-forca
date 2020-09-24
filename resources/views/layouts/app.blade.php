e7643d634d1fbecb154815865d34580afd0f8f26
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-09-24 00:12:29 -0300

    Adds telescope link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,12 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('telescope') }}">{{ __('Telescope') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
