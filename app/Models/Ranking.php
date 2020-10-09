101a48795e725a1eabf89bed02da5efe5b2a5749
Stephano Ramos <stephano.ramos@rede.ulbra.br>
2020-10-08 22:36:55 -0300

    Ajusta nome da tabela

--- app/Models/Ranking.php
+++ app/Models/Ranking.php
@@ -5,18 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Ranking
+ * @package App\Models
+ */
 class Ranking extends Model
 {
     use HasFactory;
 
-        /**
+    /**
      * @var string[]
      */
     protected $fillable = [
         'name',
         'score',
     ];
-
-
-
 }
